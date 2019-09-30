decb4ff1d065c529f92d72572fc930cba827f9f8
Mehmet Fatih Akan <akanmf@gmail.com>
2019-09-30 10:55:05 +0300

    initial commit

---
+++ HealthCare.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealthCare.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
