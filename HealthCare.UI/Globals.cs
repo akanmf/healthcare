fe8dafba7957cdc2e2461492e9ca4cc06cfa429e
Mehmet Fatih Akan <akanmf@gmail.com>
2019-09-30 15:51:46 +0300

    design has been implemented

---
+++ HealthCare.UI/Globals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthCare.UI
+{
+    public static class Globals
+    {
+        public static readonly string CONTENT_FOLDER_ROOT_PATH = "..";
+    }
+}
