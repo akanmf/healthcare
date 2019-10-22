eb1fb84d3bfa95ef57877b3b288d69cbf2ee593b
Mehmet Fatih Akan <akanmf@gmail.com>
2019-10-22 18:05:14 +0300

    localization without taghelper completed

---
+++ HealthCare.UI/Contracts/LocalizationServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthCare.UI.Contracts
+{
+    public abstract class LocalizationServiceBase
+    {
+
+        public abstract IHtmlContent Html(string key);
+        public abstract IHtmlContent Html(string key,string defaultStr);
+
+        public abstract string Translation(string key);
+        public abstract string Translation(string key,string defaultStr);
+    }
+}
